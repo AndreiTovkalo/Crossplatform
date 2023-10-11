4a0b0104e6fb40e6c06a3dfeb8275fc2315fe863
andreitovkalo <katana10070@gmail.com>
2023-10-11 21:46:43 +0300

    lab1

---
+++ CrossPlatformLabs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+string[] inputLines = File.ReadAllLines("INPUT.txt");
+int N = int.Parse(inputLines[0]);
+int[] A = inputLines[1].Split().Select(int.Parse).ToArray();
+
+Console.WriteLine(JsonSerializer.Serialize(A));
+
+// Вычисление минимального количества ходов
+int moves = 0;
+for (int i = 0; i < N; i++)
+{
+    while (A[i] > 1)
+    {
+        // Перекладываем лишние шары в соседние коробки
+        if (i < N - 1)
+        {
+            A[i]--;
+            A[i + 1]++;
+        }
+        else
+        {
+            A[i]--;
+            A[0]++;
+        }
+        moves++;
+    }
+}
+
+// Запись результата в файл OUTPUT.TXT
+File.WriteAllText("OUTPUT.TXT", moves.ToString());
